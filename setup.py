1844d431ee5325c1e5b02cf1a537a7a057620e96
aimanash <i_man_91@yahoo.com>
2022-07-15 17:54:26 +0800

    python_2_unicode_compatible #i2go_django_3

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-business-rules",
+    version="1.0.5",
+    packages=find_packages(exclude=["*.tests"]),
+    include_package_data=True,
+    license="BSD License",
+    description="Django Business Rules is a simple Django app to support business rules.",
+    long_description=README,
+    url="https://github.com/maciejpolanczyk/django-business-rules/",
+    author="Maciej Polanczyk",
+    author_email="maciej.polanczyk@gmail.com",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    install_requires=["business-rules==1.0.1", "six==1.16.0"],
+)
